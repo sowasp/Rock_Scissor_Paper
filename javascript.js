dcd7e7369a2f286239e69be38b50f3fe45902bd4
sowasp <sowsmartool@gmail.com>
2026-05-17 02:08:13 +0200

    finished the script, had some issues with committing so ill add the first commit here with the last commit too: Math.random() gives a number from: 0 up to less than 1 So i splat that range into 3 equal parts: 0.00 → 0.33   rock 0.33 → 0.66   scissors 0.66 → 1.00   paper thats the only part that gave me some issues, for the rest the code talks for it self

--- javascript.js
+++ javascript.js
@@ -9,4 +9,46 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    let choice = prompt("Choose Rock, Paper or Scissors")
+    return choice.toLowerCase()
+}
+
+
+let computerScore = 0
+let humanScore = 0
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "rock" && computerChoice === "paper"){
+        computerScore++
+        return "computer won"
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++
+        return "computer won"
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++
+        return "computer won"
+    } else if (computerChoice === "rock" && humanChoice === "paper"){
+        humanScore++
+        return "human won"
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        humanScore++
+        return "human won"
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore++
+        return "human won"
+    } else {
+        return "Tie"
+    }
+}
+
+function playGame(){
+    let result = playRound(getHumanChoice(), getComputerChoice())
+    console.log(result)
+}
+
+for(let i = 0; i < 5; i++) {
+    playGame()
+}
+
+console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`)
